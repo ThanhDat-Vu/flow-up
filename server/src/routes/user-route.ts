3b3957814993f57d64b6d5d4d5568cfc6d2c1e4a
Daniel <20521181@gm.uit.edu.vn>
2022-11-14 16:46:55 +0700

    fix: forget to change middleware name

--- server/src/routes/user-route.ts
+++ server/src/routes/user-route.ts
@@ -7,7 +7,7 @@ UserRoute.get("/get-auth-user", UserController.getAuthUser);
 UserRoute.post("/get-user-by-slug", UserController.getUserBySlug);
 UserRoute.post(
   "/get-presigned-url-for-upload",
-  UserController.getPresignedUrlForUpload
+  UserController.getPresignedUrlToUpload
 );
 UserRoute.post("/update-profile", UserController.updateUserProfile);
 
